7873468a8233dd5848ffa86ea08621dbb8e4dfd0
Faizan-Ahmad-Khan <fa3n20004@gmail.com>
2024-09-05 23:14:46 +0500

    completed the food card section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import heroimg from "../assets/heroimg.jpg"
+import FoodCard from '../components/FoodCard';
+
 
 function Home() {
-  
+
+    const foodCard =[
+        {
+            id :1,
+            imgUrl : "https://plus.unsplash.com/premium_photo-1673439304183-8840bd0dc1bf?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            name : "Pizza"
+        },
+        {
+            id :2,
+            imgUrl : "https://plus.unsplash.com/premium_photo-1683619761468-b06992704398?q=80&w=1565&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            name : "Burger"
+        },
+        {
+            id :3,
+            imgUrl : "https://plus.unsplash.com/premium_photo-1673769108070-580fe90b8de7?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            name : "Momos"
+        }
+    ]
+   
   return (
     <div id="home" > 
     <section className='relative ' >
@@ -14,6 +34,16 @@ function Home() {
         </div>
       </div>
     </section>
+
+    <section className='w-[100vw] flex flex-col justify-center items-center p-5'>
+     <h2 className='text-3xl font-bold text-gray-700 mb-8'> Explore Foods </h2>
+         <div className='w-full flex-wrap flex items-center gap-10 justify-center'>
+            { foodCard.map((item)=>(
+               <FoodCard key={item.id} name={item.name} image={item.imgUrl}/> ))
+             }
+            </div>
+        </section>
+
     </div>
   );
 }
